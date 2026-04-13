3e0c7ef411745b92d1a6aeb85c059ea73c91a8f3
TavishSK <tavishsk@gmail.com>
2026-04-14 00:50:25 +0530

    k

--- src/main/java/com/major/cloud/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.major.cloud.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/major/cloud/repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.major.cloud.repository;
-
-import com.major.cloud.model.ServiceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<ServiceEntity, Long> {
-}
